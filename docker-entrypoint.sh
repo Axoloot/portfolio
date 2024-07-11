619d133fe744466240836ef4f2edde71fa515858
Axoloot <cyril.de-lajudie@epitech.eu>
2024-07-11 19:11:07 +0200

    fix: ssh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+ls /etc/letsencrypt
 # Obtain SSL certificates if they do not exist
 if [ ! -f /etc/letsencrypt/live/therealcyril.dev/fullchain.pem ]; then
-    certbot certonly --standalone -d therealcyril.dev -d www.therealcyril.dev --non-interactive --agree-tos -m cyril_de_lajudie@hotmail.fr
+    certbot certonly --standalone -d therealcyril.dev -d www.therealcyril.dev --non-interactive --agree-tos -m cyril_de_lajudie@hotmail.fr || true
 fi
 
 # Bring Nginx back to the foreground
