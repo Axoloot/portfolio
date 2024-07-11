b70cdc287f52a5df97d037fe25d72aba51a6fb92
Axoloot <cyril.de-lajudie@epitech.eu>
2024-07-11 17:59:43 +0200

    fix: fixed email adress

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Start Nginx in the background
+nginx &
+
 # Obtain SSL certificates if they do not exist
 if [ ! -f /etc/letsencrypt/live/therealcyril.dev/fullchain.pem ]; then
-    certbot certonly --webroot --webroot-path /usr/share/nginx/html -d therealcyril.dev -d www.therealcyril.dev --non-interactive --agree-tos -m cyril_de_lajudie@hotmail.fr
+    certbot certonly --webroot --webroot-path /usr/share/nginx/html -d therealcyril.dev -d www.therealcyril.dev --non-interactive --agree-tos -m your-email@example.com
 fi
 
-# Start Nginx
+# Bring Nginx back to the foreground
 nginx -g "daemon off;"
