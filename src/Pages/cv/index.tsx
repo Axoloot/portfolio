e13d01dbd9375a2eb98fbc062f1eb336f9fbe786
Axoloot <cyril.de-lajudie@epitech.eu>
2024-11-21 03:09:43 +1100

    feat: PDF fix on mobile

--- src/Pages/cv/index.tsx
+++ src/Pages/cv/index.tsx
@@ -1,17 +1,24 @@
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { fullScreenPlugin } from '@react-pdf-viewer/full-screen';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/full-screen/lib/styles/index.css';
+
 import i18n from '../../translations';
+
+// Create new plugin instance
 
 const fr = require('../../Static/cv.pdf');
 const eng = require('../../Static/cv-eng.pdf');
 
 const CV = () => {
+  const pdfFile = i18n.language === 'fr' ? fr : eng;
+  const defaultFullScreenPlugin = fullScreenPlugin();
+
   return (
-    <iframe
-      title="cv"
-      src={i18n.language === 'fr' ? fr : eng}
-      width="100%"
-      height="100%"
-      style={{ border: 'none' }}
-    />
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/legacy/build/pdf.worker.js">
+      <Viewer fileUrl={pdfFile} plugins={[defaultFullScreenPlugin]} />
+    </Worker>
   );
 };
 
